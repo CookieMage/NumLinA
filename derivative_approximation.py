7611697d911b0b88bf4c2b661c1af30b9b533158
Bastian Hagen <141170235+CookieMage@users.noreply.github.com>
2023-10-25 11:27:46 +0200

    class FiniteDifference
    __init__, compute_dh_f, compute_ddh_f, compute_errors, main-guard
    
    es fehlen noch die 2 extramethoden

---
+++ derivative_approximation.py
@@ -0,0 +1,147 @@
+class FiniteDifference(h, f, d_f=None, dd_f=None):
+    """ Represents the first and second order finite difference approximation of a function and
+    allows for a computation of error to the exact derivatives.
+
+    Parameters
+    ----------
+    h : float
+        Step size of the approximation.
+    f : callable
+        Function to approximate the derivatives of. The calling signature is
+        'f(x)'. Here 'x' is a scalar or array_like of 'numpy'. The return
+        value is of the same type as 'x'.
+    d_f : callable, optional
+        The analytic first derivative of 'f' with the same signature.
+    dd_f : callable, optional
+        The analytic second derivative of 'f' with the same signature.
+
+    Attributes
+    ----------
+    h : float
+        Step size of the approximation.
+    """
+    #weitere attribute in den Docstring
+    h = None
+    f = None
+    d_f = None
+    dd_f = None
+    def __init__(self, h, f, d_f=None, dd_f=None):
+        """ returns an Object of class FiniteDifference
+
+        Parameters
+        ----------
+        h: float
+            Step size of the approximation.
+        f : callable
+            Function to approximate the derivatives of. The calling signature is
+            'f(x)'. Here 'x' is a scalar or array_like of 'numpy'. The return
+            value is of the same type as 'x'.
+        d_f : callable, optional
+            The analytic first derivative of 'f' with the same signature.
+        dd_f : callable, optional
+            The analytic second derivative of 'f' with the same signature.
+
+        Raises
+        ------
+        ValueError
+            One of the given arguments is of the wrong type
+        """
+        if not isinstance(h, float):
+            raise TypeError
+        if not callable(f):
+            raise TypeError
+        if d_f is not None and not callable(d_f):
+            raise TypeError
+        if dd_f is not None and not callable(dd_f):
+            raise TypeError
+        self.h = h
+        self.f = f
+        self.d_f = d_f
+        self.dd_f = dd_f
+    #wirklich richtig so???
+
+
+    def compute_dh_f(self):
+        """Calculates the approximation for the first derivative of the f with step size h.
+
+        Parameters
+        ----------
+        -
+
+        Return
+        ------
+        callable
+            Calculates the approximation of the first derivative for a given x.
+        """
+        def f_1(x):
+            return (self.f(x+self.h)-self.f(x))/self.h
+        return f_1
+
+    def compute_ddh_f(self):
+        """Calculates the approximation for the second derivative of f with step size h.
+        Parameters
+        ----------
+        -
+        Return
+        ------
+        callable
+            Calculates the approximation of the first derivative for a given x.
+        """
+        def f_2(x):
+            return (self.f(x+self.h)-2*self.f(x)+self.f(x-self.h))/self.h**2
+        return f_2
+
+    def compute_errors(self, a, b, p):
+        """ Calculates an approximation to the errors between an approximation
+        and the exact derivative for first and second order derivatives in the
+        infinity norm.
+        
+        Parameters
+        ----------
+        a, b : float
+            Start and end point of the interval.
+        p : int
+            Number of intervals used in the approximation of the infinity norm.
+
+        Returns
+        -------
+        float
+            Errors of the approximation of the first derivative.
+        float
+            Errors of the approximation of the second derivative.
+
+        Raises
+        ------
+        ValueError
+            If no analytic derivative was provided by the user.
+        """
+        f_1 = self.compute_dh_f()
+        f_2 = self.compute_ddh_f()
+        max_dif_1 = 0.0
+        max_dif_2 = 0.0
+        step = (b-a)/p
+        if self.d_f is None and self.dd_f is None:
+            raise ValueError
+        if self.d_f is not None:
+            for i in range(a, b, step):
+                max_dif_1 = max(max_dif_1, abs(f_1(i) - self.d_f(i)))
+        if self.d_f is not None:
+            for i in range(a, b, step):
+                max_dif_2 = max(max_dif_2, abs(f_2(i) - self.dd_f(i)))
+        if max_dif_1 is not None and max_dif_2 is not None:
+            return max_dif_1, max_dif_2
+        elif max_dif_2 is None:
+            return max_dif_1
+        else:
+            return max_dif_2
+
+# Methoden / Funktionen 3 un 4
+
+def main():
+    """ Example of code that can be run using the provided class, mehtods and function
+    """
+    pass
+
+if __name__=="__main__":
+    main()
+    
