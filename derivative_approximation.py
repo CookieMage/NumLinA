beff51083db44585ce7ac5533a496ab8473bef46
Bastian Hagen <141170235+CookieMage@users.noreply.github.com>
2023-10-29 13:45:11 +0100

    almost done

--- derivative_approximation.py
+++ derivative_approximation.py
@@ -1,9 +1,9 @@
-import manage_list as ml
+import math
+import numpy as np
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
-from math import sin
-import matplotlib
-import re
-import os
+import manage_list as ml
+
 
 
 class FiniteDifference:
@@ -33,7 +33,7 @@ class FiniteDifference:
     f = None
     d_f = None
     dd_f = None
-    
+
     def __init__(self, h, f, d_f=None, dd_f=None):
         """ returns an Object of class FiniteDifference
 
@@ -69,7 +69,6 @@ class FiniteDifference:
         self.f = f
         self.d_f = d_f
         self.dd_f = dd_f
-    #wirklich richtig so???
 
 
     def compute_dh_f(self):
@@ -84,8 +83,8 @@ class FiniteDifference:
         callable
             Calculates the approximation of the first derivative for a given x.
         """
-        def f_1(y : float):
-            return (self.f(y+self.h)-self.f(y))/self.h
+        def f_1(x : float):
+            return (self.f(x+self.h)-self.f(x))/self.h
         return f_1
 
     def compute_ddh_f(self):
@@ -98,8 +97,8 @@ class FiniteDifference:
         callable
             Calculates the approximation of the first derivative for a given x.
         """
-        def f_2(x):
-            return (self.f(x+self.h)-2*self.f(x)+self.f(x-self.h))/self.h**2
+        def f_2(x : float):
+            return (self.f(x+self.h)-2*self.f(x)+self.f(x-self.h))/(self.h**2)
         return f_2
 
     def compute_errors(self, a, b, p):
@@ -132,38 +131,25 @@ class FiniteDifference:
         f_2 = self.compute_ddh_f()
         max_dif_1 = 0.0
         max_dif_2 = 0.0
-        # Bei der Brechnung von step muss p-1 genutz werden, damit später b+1 in range verwendet
-        # werden kann und die korrekte Anzahl an Intervallen vorhanden bleibt
-        step = ((2*abs(b-a)) // (p-1))
-        if step == 0:
-            step = 1
-        # Es wird 2*abs(b-a)+1 als intervallende genutzt, damit wir durch 0.5*x eine höhere
-        # Genauigkeit der Intervallaufteilung genutzt werden kann und range() kein abgeschlossenes
-        # Intervall erzeugt. Damit b also im Intervall sein kann müssen wir das Intervallende um 1
-        # erhöhen. Dies funktioniert, da range() nur ganze Zahlen erzeugen kann.
-        array = [a+0.5*x for x in range(0, 2*abs(b-a)+1, step)]
+        steps = np.linspace(a, b, p)
         if self.d_f is not None:
-            for i in array:
+            for i in steps:
                 max_dif_1 = max(max_dif_1, abs(f_1(i) - self.d_f(i)))
         if self.d_f is not None:
-            for i in array:
+            for i in steps:
                 max_dif_2 = max(max_dif_2, abs(f_2(i) - self.dd_f(i)))
         return max_dif_1, max_dif_2
 
 
     def experiment(self, func_name, a, b, p):
-        step = ((2*abs(b-a)) // (p-1))
-        if step == 0:
-            step = 1
-        array = [a+0.5*x for x in range(0, 2*abs((b+1)-a), step)]
+        steps = np.linspace(a, b, p)
         dh_f = self.compute_dh_f()
         ddh_f = self.compute_ddh_f()
         errors = self.compute_errors(a, b, p)
         ml.save(func_name, "_error.csv", "dh_1", self.h, errors[0])
         ml.save(func_name, "_error.csv", "ddh_2", self.h, errors[1])
         ml.save(func_name, "_error.csv", "")
-        for i in array:
-            ml.save(func_name, ".csv", i, self.f(i))
+        for i in steps:
             ml.save(func_name, "_dh.csv", i, dh_f(i))
             ml.save(func_name, "_ddh.csv", i, ddh_f(i))
             ml.save(func_name, "_1.csv", i, self.d_f(i))
@@ -178,11 +164,11 @@ class FiniteDifference:
 
 
 def plothelper(filename,):
-    with open("experiments/" + filename, "r") as file:
+    with open("experiments/" + filename, "r", encoding = "utf8") as file:
         lines = file.readlines()
         lines = [i.split() for i in lines]
         lines_list = []
-        
+
         if "error" not in filename:
             counter = 0
             for i,e in enumerate(lines):
@@ -200,9 +186,9 @@ def plothelper(filename,):
             for i,e in enumerate(y):
                 for j,f in enumerate(e):
                     y[i][j] = float(f)
+            x = x[0]
             for i,e in enumerate(x):
-                for j,f in enumerate(e):
-                    x[i][j] = float(f)
+                x[i] = float(e)
             return x, y
         else:
             counter = 0
@@ -230,66 +216,90 @@ def plothelper(filename,):
             dh = [inner for outer in dh for inner in outer]
             ddh = [inner for outer in ddh for inner in outer]
             for i, e in enumerate(dh):
-                    dh[i] = float(e)
+                dh[i] = float(e)
             for i, e in enumerate(ddh):
-                    ddh[i] = float(e)
+                ddh[i] = float(e)
             return h, dh, ddh
 
 
 
 def plot(title, filename_1, name_1, filename_2 = None, name_2 = None, filename_3 = None, name_3 = None, filename_4 = None, name_4 = None):
-    fig1, ax1 = plt.subplots(figsize=(5, 5))
-    plt.yscale("log")
-    plt.xscale("log")
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
+    _, ax1 = plt.subplots(figsize=(5, 5))
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    plt.xlabel("x", fontsize = 20)
+    ax1.xaxis.set_label_coords(1.02, 0.025)
+    ax1.yaxis.get_offset_text().set_fontsize(20)
     plt.title(title, fontsize=40)
     ax1.grid()
-    
+    legend = []
+    graphs = []
+
     y1 = []
     if "error" not in filename_1:
         x, y1 = plothelper(filename_1)
-        plt.plot(x, y1, "b", label = name_1, linewidth = 2, linestyle="solid")
-    else:
-        h, dh, ddh = plothelper(filename_1)
-        plt.plot(h, dh, "b", label = name_1, linewidth = 2, linestyle="solid")
-        plt.plot(h, ddh, "g", label = name_1, linewidth = 2, linestyle="solid")
+        for e in y1:
+            print(max(e))
+            plt.plot(x, e, "b", linewidth = 2, linestyle="dashed")
+        legend.append(name_1)
+        graphs.append(Line2D([0], [0], color = "b", linewidth=2, linestyle="dashed"))
 
-        
-    
-    if filename_2 != None:
-        if "error" not in filename_1:
+        if filename_2 is not None:
             y2 = []
             x, y2 = plothelper(filename_2)
-            plt.plot(x, y2, "g", label = name_2, linewidth = 2, linestyle="dashed")
-        else:
-            h, dh, ddh = plothelper(filename_1)
-            plt.plot(h, dh, "b", label = name_1, linewidth = 2, linestyle="dashed")
-            plt.plot(h, ddh, "g", label = name_1, linewidth = 2, linestyle="dashed")
+            for e in y2:
+                plt.plot(x, e, "g", linewidth = 2, linestyle="solid")
+            legend.append(name_2)
+            graphs.append(Line2D([0], [0], color = "g", linewidth=2, linestyle="solid"))
 
-        
-    if filename_3 != None:
-        if "error" not in filename_1:
+        if filename_3 is not None:
             y3 = []
             x, y3 = plothelper(filename_3)
-            plt.plot(x, y3, "r", label = name_3, linewidth = 2, linestyle="dotted")
-        else:
-            h, dh, ddh = plothelper(filename_1)
-            plt.plot(h, dh, "b", label = name_1, linewidth = 2, linestyle="dotted")
-            plt.plot(h, ddh, "g", label = name_1, linewidth = 2, linestyle="dotted")
-        
-    if filename_4 != None:
-        if "error" not in filename_1:
+            for e in y3:
+                plt.plot(x, e, "r", linewidth = 2, linestyle="dotted")
+            legend.append(name_3)
+            graphs.append(Line2D([0], [0], color = "r", linewidth=2, linestyle="dotted"))
+
+        if filename_4 is not None:
             y4 = []
             x, y4 = plothelper(filename_4)
-            plt.plot(x, y4, "y", label = name_4, linewidth = 2, linestyle="dashdot")
-        else:
-            h, dh, ddh = plothelper(filename_1)
-            plt.plot(h, dh, "b", label = name_1, linewidth = 2, linestyle="dashdot")
-            plt.plot(h, ddh, "g", label = name_1, linewidth = 2, linestyle="dashdot")
+            for e in y4:
+                plt.plot(x, e, "y", linewidth = 2, linestyle="dashdot")
+            legend.append(name_4)
+            graphs.append(Line2D([0], [0], color = "y", linewidth=2, linestyle="dashdot"))
+    else:
+        plt.xlabel("h", fontsize = 20)
+        ax1.xaxis.set_label_coords(1.02, 0.025)
+        plt.yscale("log")
+        plt.xscale("log")
+        h, dh, ddh = plothelper(filename_1)
+        plt.plot(h, dh, "b", label = "dh", linewidth = 2, linestyle="solid")
+        plt.plot(h, ddh, "g", label = "ddh", linewidth = 2, linestyle="solid")
+        legend.append("dh")
+        graphs.append(Line2D([0], [0], color = "b", linewidth=2))
+        legend.append("ddh")
+        graphs.append(Line2D([0], [0], color = "g", linewidth=2))
+    
     plt.setp(ax1.get_xticklabels(), rotation=40, horizontalalignment='right')
-    plt.legend(fontsize=20)
+    plt.legend(graphs, legend, fontsize=20, loc="upper left")
     plt.show()
+
+def power_func(numbers : list):
+    colors = ["b", "g", "y"]
+    line = ["solid", "dashed", "dotted"]
+    for s in [1, 2, 3]:
+        #labels = [f"x^{s}", f"{s}x^{s-1}", f"{s-1*s}x^{s-2}"]
+        labels = [f"f_{s}", f"f_{s}'", f"f_{s}''"]
+        def f(x):
+            return x**s
+        def f_1(x):
+            return s*x**(s-1)
+        def f_2(x):
+            return (s-1)*s*x**(s-2)
+        for i,e in enumerate([f, f_1, f_2]):
+            plt.plot(numbers, [e(x) for x in numbers], label = labels[i],
+                     color = colors[s-1], linewidth = 2, linestyle = line[i])
+            plt.legend(fontsize = 20)
 
 
 def progress_bar(current, total, bar_length=20):
@@ -335,15 +345,14 @@ def progress_bar(current, total, bar_length=20):
         raise exc
 
 
-# Methoden / Funktionen 3 un 4
 
 def main():
     """ Example of code that can be run using the provided class, mehtods and function
     """
-    
-    a = -1000
-    b = 1000
+    a = math.pi
+    b = 3*math.pi
     p = 10
+    c = 0.1
 
     for func_name in ["g", "gk"]:
         for file in ["_1", "_2", "_dh", "_ddh", "_error", ""]:
@@ -351,37 +360,37 @@ def main():
 
 
     def g(x):
-        return sin(x)/x
+        return math.sin(x)/x
     def d_g(x):
-        return 0
+        return math.cos(x)/x - math.sin(x)/x**2
     def dd_g(x):
-        return 0
+        return -((x**2-2)*math.sin(x)+2*x*math.cos(x))/x**3
 
-    def gk(x, k=1):
-        return sin(k*x)/x
-    def d_gk(x, k=1):
-        return 0
-    def dd_gk(x, k=1):
-        return 0
+    def gk(x, k=c):
+        return math.sin(k*x)/x
+    def d_gk(x, k=c):
+        return (k*math.cos(k*x)/x) - (math.sin(k*x)/x**2)
+    def dd_gk(x, k=c):
+        return -((k**2*x**2-2)*math.sin(k*x)+2*k*x*math.cos(k*x))/x**3
 
 
 
-    for i in range(2, 16):
+    for i in range(1, 16):
         h = 10**(-i)
         exp_1 = FiniteDifference(h, g, d_g, dd_g)
         exp_1.experiment("g", a, b, p)
-    for i in range(2, 16):
+    for i in range(1, 16):
         h = 10**(-i)
         exp_2 = FiniteDifference(h, gk, d_gk, dd_gk)
         exp_2.experiment("gk", a, b, p)
 
 
-    plot("1. Ableitung", "g_1.csv", "g'", "g_dh.csv", "dh")
-    plot("2. Ableitung", "g_2.csv", "g''", "g_ddh.csv", "ddh")
-    plot("Abschätzungsfehler", "g_error.csv", "g'", "g_dh.csv", "dh")
-    plot("1. Ableitung", "g_1.csv", "g'", "g_dh.csv", "dh")
-    plot("2. Ableitung", "g_2.csv", "g''", "g_ddh.csv", "ddh")
-    plot("Abschätzungsfehler", "g_error.csv", "g''", "g_dh.csv", "ddh")
+    plot("1. Ableitung", "g_dh.csv", "dh", "g_1.csv", "g'")
+    plot("2. Ableitung", "g_ddh.csv", "ddh", "g_2.csv", "g''" )
+    plot("Abschätzungsfehler", "g_error.csv", "")
+    plot("1. Ableitung", "gk_dh.csv", "dh", "gk_1.csv", "g'")
+    plot("2. Ableitung", "gk_ddh.csv", "ddh", "gk_2.csv", "g''")
+    plot("Abschätzungsfehler", "gk_error.csv", "")
 
 
 if __name__=="__main__":
--- manage_list.py
+++ manage_list.py
@@ -2,13 +2,6 @@
 
 Functions
 ---------
-create_list()
-    function for creating files containing lists of intergers
-create_word_list()
-    function for creating files containing lists of strings
-get_list()
-    function that reads a file created by one of the first functions and returning its content
-    as a list
 save()
     saves data in a specific format
 clear()
@@ -19,22 +12,25 @@ main():
 
 import os
 
-def save(func_name, filename, data, data_1="", data_2=""):
-    '''function that saves data in a specific format
+def save(func_name : str, filename : str, data : str, data_1="", data_2=""):
+    '''function that appends data in a specific format to a specified file
     
     Parameters
     ----------
+    func_name: str
+        name of thefunction which produced the data
+        file-prefix of file in which the data is saved
     filename : str
-        file in which the data is saved
-    iteration : int
-        number which is saved in order to make the file readable for a human
-    data : int or list
+        file-suffix of file in which the data is saved
+    data : int or list or str
+        data that is saved
+    data_1 : int or list or str, optional
+        data that is saved
+    data_2 : int or list or str, optional
         data that is saved
     
     Exceptions
     ----------
-    OSError
-        file does not exist
     TypeError
         filename is not a str
     '''
@@ -45,25 +41,26 @@ def save(func_name, filename, data, data_1="", data_2=""):
         # haenge die Parameter im Format "iteration data\n" hinten an das Dokument filename an
         with open("experiments/" + func_name + filename, "a", encoding="utf8") as file:
             file.write(str(data)+" "+str(data_1)+" "+str(data_2)+"\n")
-    except OSError as exc:
-        raise exc
     except TypeError as exc:
         raise exc
 
 
 def clear(func_name, filename):
-    '''function for clearing files
+    '''function for clearing files; if the file did not exist an empty one will be created
+    
     Parameters
     ----------
+    func_name: str
+        name of the function which produced the data that is to be deleted
+        file-prefix of file in which the data is be deleted
     filename : str
-        file that is to be cleared
+        file-suffix of file in which the data is to be deleted
     
     Exceptions
     ----------
-    OSError
-        file does not exist
     TypeError
         filename is not a str
+        func_name is not a str
     '''
     try:
         cwd = os.getcwd()
@@ -72,35 +69,19 @@ def clear(func_name, filename):
         with open("experiments/" + func_name + filename, "w", encoding="utf8") as file:
             # ueberschreibe das Dokument mit einem leeren str
             file.write("")
-    except OSError as exc:
-        raise exc
     except TypeError as exc:
         raise exc
 
 def main():
     '''example implementation for using the above defined code
     '''
-    # erstelle eine Zahlenliste
-    create_list(50)
-    # lese diese Liste aus und gebe sie aus
-    print(get_list("list.txt"))
-    # erstelle eine Woerterliste
-    create_word_list(50)
-    # lese diese aus und speichere sie in word_list
-    word_list = get_list("list.txt")
-    # gebe die Liste aus
-    print(word_list)
-    # leere das Dokument
-    clear("list.txt")
-    # gebe das leere Dokument aus
-    print(get_list("list.txt"))
-    # speichere die Daten der Woerterliste mittels save
-    for i, element in enumerate(word_list):
-        save("list.txt", i, element)
-    # gebe den Inhalt des Dokuments aus
-    print(get_list("list.txt"))
+    # speichere Daten mittels save
+    for i, element in enumerate([1, "data", "list", [1, 2]]):
+        save("1_", "list.txt", i, element)
+    clear("1_", "list.txt")
+    for i, element in enumerate([2, "data", "list", [3, 4]]):
+        save("1_", "list.txt", i, element)
 
 # main-guard
 if __name__ == "__main__":
     main()
-    
