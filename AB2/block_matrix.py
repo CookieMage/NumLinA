75171ff1dd2bc1cadc73809ce2fad34b1bad0c90
Bastian Hagen <141170235+CookieMage@users.noreply.github.com>
2023-11-24 15:57:45 +0100

    Matrizen
    werden endlich richtig erzeugt und gespeichert
    Frage: wo soll die Matrix erzeugt werden? Repräsentiert ein Objekt eine d für mehrere _d oder ist ^d=_d?

--- AB2/block_matrix.py
+++ AB2/block_matrix.py
@@ -4,26 +4,40 @@ from scipy import sparse
 class BlockMatrix:
     d = None # int
     n = None # int
-    a_1 = None # coo_array
-    a_2 = None # coo_array
-    a_3 = None # coo_array
+    a_d = None # coo_array
+
     def __init__(self, d, n):
         if d < 1 or d > 3:
             raise ValueError
         self.d = d
         self.n = n
-        self.a_1 = sparse.diags([-1, 2*self.d, -1], [-1, 0, 1], shape=(self.n-1, self.n-1), format = "coo")
+        a_1 = sparse.diags([-1, 2*self.d, -1], [-1, 0, 1], shape=(self.n-1, self.n-1))
+        if d == 1:
+            self.a_d = a_1
+        else:
+            # generiere blockmatrix, welche a_1 enthält
+            a_2_block = sparse.block_diag([a_1 for _ in range(self.n-1)])
+            # generiere Blockmatrix, welche die Identitätsmatrizen enthält
+            a_2_ident = sparse.diags([-1, -1], [-(self.n-1), self.n-1], shape=((self.n-1)**2, (self.n-1)**2))
+            # addiere die Matrizen, um die gesuchte Matrix a_2 zu erhalten
+            a_2 = a_2_block + a_2_ident
+            if d == 2:
+                self.a_d = a_2
+            else:
+                # generiere blockmatrix, welche a_2 enthält
+                a_3_block = sparse.block_diag([a_2 for _ in range(self.n-1)])
+                # generiere Blockmatrix, welche die Identitätsmatrizen enthält
+                a_3_ident = sparse.diags([-1, -1], [-(self.n-1)**2, (self.n-1)**2], shape=((self.n-1)**3, (self.n-1)**3))
+                # addiere die Matrizen, um die gesuchte Matrix a_2 zu erhalten
+                a_3 = a_3_block + a_3_ident
+                self.a_d = a_3
+        
+        # a_d_block = sparse.block_diag([a_1 for _ in range((self.n-1)**d)])
+        # a_d_ident = sparse.diags([-1, -1], [-(self.n-1)**(d-1), (self.n-1)**(d-1)], shape=((self.n-1)**d, (self.n-1)**d))
+        
+        
     def get_sparse(self):
-        print("No")
-        # data = np.array([2*self.d, -1] + [0 for _ in range(2, self.n)])
-        #offset = np.array([])
-        #a_1 = sparse.diags([-1, 2*self.d, -1], [-1, 0, 1], shape=(self.n-1, self.n-1)).toarray()
-        #print(a_1)
-        #print(-1*sparse.identity((self.n-1)**1), a_1, -1*sparse.identity((self.n-1)**1))
-        #a_1_id = sparse.hstack(-1*sparse.identity((self.n-1)**1), a_1)#, -1*sparse.identity((self.n-1)**1))
-        #print(a_1_id)
-        #a_2 = sparse.block_diag((a_1_id)).toarray()
-        #print(a_2)
+        return self.a_d.toarray()
     
     def eval_sparsity(self):
         abs_non_zero = self.a_1.count_nonzero()
@@ -31,6 +45,7 @@ class BlockMatrix:
         rel_non_zero = abs_non_zero / abs
         return abs_non_zero, rel_non_zero
     
-block = BlockMatrix(2, 10)
-print(block.a_1.toarray())
-print(block.eval_sparsity())
+block = BlockMatrix(3, 3)
+
+print("------------")
+print(block.get_sparse())
