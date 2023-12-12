bb53ad117645562db827d8113f3cff613d01dcca
Erik-wltr <mail@erik-walter.de>
2023-12-12 21:25:41 +0100

    implimetierung von solve.triangular
    Hab unser solve_lu_alt weiter getestetet. Die Lösung ist identisch zu solve:_lu(scipy)

--- AB2/linear_solvers.py
+++ AB2/linear_solvers.py
@@ -1,8 +1,9 @@
 import numpy as np
-import scipy
+from scipy import sparse
+from scipy import linalg
 from block_matrix import BlockMatrix
 
-def solve_lu(p : np.ndarray, l : np.ndarray, u : np.ndarray, b : np.ndarray):
+def solve_lu_alt(p : np.ndarray, l : np.ndarray, u : np.ndarray, b : np.ndarray):
     """ Solves the linear system Ax = b via forward and backward substitution
     given the decomposition A = p * l * u.
 
@@ -25,7 +26,7 @@ def solve_lu(p : np.ndarray, l : np.ndarray, u : np.ndarray, b : np.ndarray):
     p_t = p.transpose() #p.t geht vlt auch
     z = p_t.dot(b)
     print("z=",z)
-    
+
     #Lösen von Ly = z rekrusiv
     y = [0] * len(b)    #np.ndarray(shape=(0,len(b)))
     y[0] = z[0]/ l[0][0]
@@ -50,6 +51,35 @@ def solve_lu(p : np.ndarray, l : np.ndarray, u : np.ndarray, b : np.ndarray):
     #jetzt sollten wir den Vektor y berechnet haben
     #als nächstes lösen wir u * x = y rekrusiv
 
+def solve_lu(p : np.ndarray, l : np.ndarray, u : np.ndarray, b : np.ndarray):
+    """ Solves the linear system Ax = b via forward and backward substitution
+    given the decomposition A = p * l * u.
+
+    Parameters
+    ----------
+    p : numpy.ndarray
+        permutation matrix of LU-decomposition
+    l : numpy.ndarray
+        lower triangular unit diagonal matrix of LU-decomposition
+    u : numpy.ndarray
+        upper triangular matrix of LU-decomposition
+    b : numpy.ndarray
+        vector of the right-hand-side of the linear system
+
+    Returns
+    -------
+    x : numpy.ndarray
+        solution of the linear system
+    """
+    p_t = p.transpose()
+    
+    z = p_t.dot(b)
+    print("z=",z)
+    y = linalg.solve_triangular(l,z,trans = 0,lower = True)
+    print("y scipy=", y)
+    x = linalg.solve_triangular(u,y,trans=0,lower = False)
+    return np.array(x)
+
 
 
 p = np.array([[0,0,0,1],
@@ -66,4 +96,5 @@ l= np.array([[1,0,0,0],
              [1/4,1/2,1,0],
              [1/2,1/4,-1/4,1]])
 
-print(solve_lu(p, l, u, b))
+print("Lösung von uns" , solve_lu_alt(p, l, u, b))
+print("Lösung von scipy", solve_lu(p, l, u, b))
